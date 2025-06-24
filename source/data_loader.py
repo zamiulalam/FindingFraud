a72e4736b1f604a06b3fbbe2d03b472e158061a8
RJ <microsoft.greasily935@passinbox.com>
2025-06-24 21:46:04 +0200

    Add some new functions for data pruning

--- source/data_loader.py
+++ source/data_loader.py
@@ -3,12 +3,24 @@ from sklearn.impute import IterativeImputer
 import pandas as pd
 from os.path import join as join_path
 import numpy as np
+from dataclasses import dataclass
+
+@dataclass
+class TextColor:
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    RESET = "\033[0m"
 
 class DataLoader():
     def __init__(self, transaction=True):
-        self._data_path = "../../data/"
-        self._transaction = transaction
-        self.df = None
+        self._data_path: str = "../../data/"
+        self._transaction: bool = transaction
+        self.df: pd.DataFrame|None = None
+        self.float_cols: list[str] = []
+        self.int_cols: list[str] = []
+        self.str_cols: list[str] = []
+        self.bool_cols: list[str] = []
 
     @property
     def df(self):
@@ -70,7 +82,6 @@ class DataLoader():
 
             self.df.at[i, 'IsDuplicateInWindow'] = match_count
         
-    
     def reduce_mem_usage(self):
         """
         Downcast all object types to smallest type able to represent the range in the dataset
@@ -88,7 +99,21 @@ class DataLoader():
 
         self.df.info(memory_usage='deep')
 
-    def encode_AG(self, groupby:str, aggregate_cols:list):
+    def encode_AG(self, groupby: str, aggregate_cols: list[str])->None:
+        """Add new mean and standard deviation columns for a given set of columns
+
+        Parameters
+        -----------------------------------
+        groupby: str
+            The column that is used to group the dataframe. 
+        aggregate_cols: list[str]
+            The list of columns for which the aggregate values are to computed.
+
+        Returns
+        -----------------------------------
+        None
+            The dataframe is modified to add new columns for mean and standard deviation of the columns to be aggregated.
+        """
 
         # Compute the mean and std only for those columns
         means = self.df.groupby(groupby)[aggregate_cols].transform('mean').add_suffix('_uid_mean')
@@ -97,19 +122,25 @@ class DataLoader():
         # Concatenate the results with the original dataframe
         self.df = pd.concat([self.df, means, stds], axis=1)
 
-    def encode_FE(self, cols:list, inplace=False):
-        """
-        input:
-        cols: list of features to frequency encode
-        inplace: replace the columns with their frequency encoding. 
-        output:
-        dataframe with frequency encoded features
+    def encode_FE(self, cols: list[str], inplace: bool = False)->None:
+        """Modifies the pandas dataframe with frequency encoding either inplace or by adding new columns.
+
+        Parameters
+        -----------------------------------
+        cols: list[str] 
+            List of features to frequency encode
+        inplace: bool
+            A flag which when set to True replace the columns with their frequency encoding. 
+    
+        Returns
+        -----------------------------------
+        None
+            The dataframe is modified with frequency encoded features
         """
         # cols = str or list of str
         if isinstance(cols, str):
             cols = [cols]
 
-        
         for col in cols:
             if inplace:
                 col_name = col
@@ -118,11 +149,20 @@ class DataLoader():
             freq = self.df[col].value_counts(normalize=True)
             self.df[col_name] = self.df[col].map(freq)
 
-    
-    def iterative_imputation(self, max_iter=10, random_state=42):
-        """
-        Impute missing values in self.df using IterativeImputer.
-        Assumes all columns are numeric or already encoded.
+    def iterative_imputation(self, max_iter: int = 10, random_state: int = 42)->None:
+        """Impute missing values in self.df using IterativeImputer.Assumes all columns are numeric or already encoded.
+
+        Parameters
+        -----------------------------------
+        max_iter: int
+            The maximum number of iterations to use for the imputer
+        random_state: int
+            The random state to be used by the imputer. Set it to get reproducible results
+
+        Returns
+        -----------------------------------
+        None
+            Modified pandas dataframe object in place
         """
         columns = self.df.columns
         index = self.df.index
@@ -133,16 +173,28 @@ class DataLoader():
         self.df = pd.DataFrame(imputed_array, columns=columns, index=index)
     
     
-    def load_csv(self, transaction_file=None, identity_file=None, 
-                 tr_columns:str=None, id_columns:str=None):
+    def load_csv(self, transaction_file: str|None = None, identity_file: Optional[str] = None, 
+                 tr_columns: str|None = None, id_columns: str|None = None,
+                 isTest: bool = False)->None:
         
         """
-        transaction_file:   path to transaction csv
-        identity_file:      path to identity file. Optional. If included it will be merged with transaction dataframe
-        tr_columns:         path to file containing names of all transaction columns to include. If left blank all columns will be loaded
-        id_columns:         path to file containing names of all id columns to include. If left blank all columns will be loaded
+        Parameters
+        -----------------------------------
+        transaction_file: str|None
+            Path to transaction csv
+        identity_file: Optional[str]
+            Path to identity file. Optional. If included it will be merged with transaction dataframe
+        tr_columns: str|None
+            Path to file containing names of all transaction columns to include. If left blank all columns will be loaded
+        id_columns: str|None
+            Path to file containing names of all id columns to include. If left blank all columns will be loaded
+        isTest: bool
+            A flag to specify whether the csv being loaded is test sample or train sample
 
-        returns: None. Modifies dataframe in place
+        Returns
+        -----------------------------------
+        None 
+            Modifies dataframe in place
         """
 
         # Read in list of columns to use
@@ -151,7 +203,9 @@ class DataLoader():
             with open(tr_columns) as f:
                 usecols = f.readlines()
             usecols = [item.strip() for item in usecols]
-
+        if not isTest and 'isFraud' not in usecols:
+            print(f"{TextColor.YELLOW}The columns supplied do not have isFraud column which is needed for training, adding it{TextColor.RESET}")
+            usecols.append('isFraud')
         # Open transaction file
         with open(join_path(self._data_path, transaction_file)) as f:
 
@@ -168,6 +222,9 @@ class DataLoader():
             with open(id_columns) as f:
                 usecols = f.readlines()
             usecols = [item.strip() for item in usecols]
+            if 'TransactionID' not in usecols:
+                print(f"{TextColor.YELLOW}The ID columns do not have TransactionID column. Adding it for merging dataframes{TextColor.RESET}")
+                usecols.append("TransactionID")
 
         # Read ID file
         if identity_file:
@@ -187,3 +244,24 @@ class DataLoader():
        
         self.reduce_mem_usage()
         return self.df
+
+    def getListOfColTypes(self):
+        """
+        Adds the columns of a certain data type into corresponding list so that it is easily accessible. Useful for encoding, for example. 
+
+        Only call this function after the load_csv method has been called on an instance of the class.
+        """
+        if self.df is None:
+            print(f"{TextColor.RED}The dataframe is None! Please make sure you have called load_csv method first and got a valid pandas dataframe. Returning empty lists{TextColor.RESET}")
+            return
+        for col, col_type in self.df.dtypes.to_dict().items():
+            if col_type in ['float16', 'float32', 'float64']:
+                self.float_cols.append(col)
+            elif col_type in ['int8', 'int16', 'int32', 'int64']:
+                self.int_cols.append(col)
+            elif col_type == 'object':
+                self.str_cols.append(col)
+            elif col_type == 'bool':
+                self.bool_cols.append(col)
+            else:
+                print(f"column {col} is not one of the [float64, int64, object, bool] types. It's type is {col_type} ")
